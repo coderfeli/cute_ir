ee426bd57ced851b2a672a45c93e1bebcce2e324
coderfeli <coderfeli@163.com>
2025-11-25 22:53:01 +0800

    remove useless

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p build && cd build
 cmake .. -DMLIR_DIR=/mnt/raid0/felix/llvm-project/buildmlir/lib/cmake/mlir
-make -j; make rocir-opt -j; make _rocirDialect -j;
+make -j; make rocir-opt -j; make _rocirDialect -j;  make RocirPythonOpsIncGen -j;
 cd -;
 cd python;
 python setup.py develop;
--- build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p build && cd build
-cmake .. -DMLIR_DIR=/mnt/raid0/felix/llvm-project/buildmlir/lib/cmake/mlir
-make -j; make rocir-opt -j; make _rocirDialect -j;  make RocirPythonOpsIncGen -j;
-cd -;
-cd python;
-python setup.py develop;
-cd -;
-sh run_tests.sh;
