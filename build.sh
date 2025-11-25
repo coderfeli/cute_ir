d2489e87930e7409db86a172bf524d188b6688b0
coderfeli <coderfeli@163.com>
2025-11-25 01:12:59 +0000

    add build file

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p build && cd build
+cmake .. -DMLIR_DIR=/mnt/raid0/felix/llvm-project/buildmlir/lib/cmake/mlir
+make -j; make cute-opt -j;
+cd -;
+cd python;
+python setup.py develop;
+cd -;
+sh run_tests.sh;
